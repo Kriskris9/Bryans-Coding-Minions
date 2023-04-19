171ddbed10e0b88c74d6097c3dcfc5806b285e11
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-19 16:46:22 -0700

    finished networking-route.js

---
+++ controllers/api/networking-routes.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const { Networking, User } = require('../../models');
+
+//these are the routes for the api/memes endpoint
+//the api/memes endpoint
+
+router.get('/', async (req, res) => {
+  //find all networking events
+  //include the associated user data
+  try {
+    const networkingData = await Networking.findAll({
+      include: [{ model: User }]
+    });
+    res.status(200).json(networkingData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new networkingEvent
+  try {
+    const networkingData = await Memes.create(req.body);
+    res.status(200).json(networingData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
+});
+
+router.delete('/:id', async (req, res) => {
+  //delete a networking event by its id value
+  try {
+    const networkingData = await Networking.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!netWorkingData) {
+        res.status(404).json({ message: 'No networking event found with this id!' });
+
+      return;
+    }
+    res.status(200).json(networkingData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+});
+
+router.put('/:id', async (req, res) => {
+  //update a networking event by its id value
+  try {
+    const networkingData = await Networking.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!networkingData) {
+ res.status(404).json({ message: 'No networking event found with this id!' });
+      return;
+    }
+    res.status(200).json(networkingData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+});
+
+
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 
 module.exports = router;
+
+
+// require homeroutes and have router.use homeroutes
