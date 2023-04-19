2b9066d30fbc73f29153e547bc4129f3d8768ee0
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-19 13:15:40 -0700

    finished index.js for models folder

---
+++ models/index.js
@@ -0,0 +1,56 @@
+const Chat = require('./Chat');
+const Memes = require('./Memes');
+const Study = require('./Study');
+const User = require('./User');
+const Networking = require('./Networking');
+
+
+User.hasMany(Study, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+
+
+Study.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+User.hasMany(Memes, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+
+
+
+Memes.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+User.hasMany(Chat, {
+  foreignKey: 'user_id',
+
+});
+
+
+Chat.belongsTo(User, {
+
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Networking, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+
+
+Networking.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+module.exports = { User, Study, Memes, Chat, Networking };
