31c7be3f9c21c131aa86ee73da73f76216911e80
Joseph Baek <josephbaek97@gmail.com>
2023-04-20 11:28:21 -0700

    changes to handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const userRoutes =require('./user-routes')
 const networkingRoutes = require('./networking-routes')
 const memesRoutes= require ('./memes-routes')
+const chatRoutes = require('./chat-routes')
 
 // const studyRoutes = require('./study-routes')
 
 router.use('/user', userRoutes);
 router.use('/networking', networkingRoutes);
 router.use('/memes', memesRoutes);
+router.use('/chat', chatRoutes);
 
 // router.use('/study', studyRoutes);
 
