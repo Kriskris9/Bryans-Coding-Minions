ee519a74107ab1e3436dcc5248076723b4d49d2b
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-20 14:05:51 -0700

    fixed server.js file and added req.session.userid

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
