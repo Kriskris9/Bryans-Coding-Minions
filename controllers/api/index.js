754158bd31f1a99c4404880560ce81d5a212f0a0
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-20 10:23:46 -0700

    added study routes to controllers folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const userRoutes =require('./user-routes')
 const networkingRoutes = require('./networking-routes')
 const memesRoutes= require ('./memes-routes')
+
 // const studyRoutes = require('./study-routes')
 
 router.use('/user', userRoutes);
 router.use('/networking', networkingRoutes);
 router.use('/memes', memesRoutes);
+
 // router.use('/study', studyRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,6 +13,7 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+});
 
 router.get('/study', async (req, res) => {
     try {
