1005cd4e9cae0967251f302dda29def61edacb81
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-21 11:14:32 -0700

    changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,7 @@ const mediaSelect = document.querySelector('#media');
 const mediaInput = document.querySelector('#media-input');
 const mediaFileInput = document.querySelector('#media_file');
 const chats = document.querySelectorAll('.chat-details');
+const submit = document.querySelectorAll('button');
 
 // Add event listener to hide/show the media input field
 mediaSelect.addEventListener('change', () => {
@@ -14,5 +15,21 @@ mediaSelect.addEventListener('change', () => {
   } else {
     mediaInput.style.display = 'block';
   }
+});
+
+// Add an event listener to submit a new chat
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const user = userNameInput.value
+    const message = messageInput.value
+    const media = mediaInput.value
+    const mediaFile = mediaFileInput.files[0]
+    chats = {
+        user,
+        message,
+        media,
+        mediaFile
+    }
 });
 
