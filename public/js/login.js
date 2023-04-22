a45f7e779c5fbbf7dac2527350f962daf5b7a211
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-22 13:12:05 -0700

    do not pull

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/user/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -36,10 +36,11 @@ const loginFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/');
       } else {
-        alert('Failed to sign up.');
+          alert('Failed to sign up.');
+        }
       }
     }
-  };
+  
   
   document
     .querySelector('.login-form')
